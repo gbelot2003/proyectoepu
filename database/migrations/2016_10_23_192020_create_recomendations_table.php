242bb439556c5ddbdce788e6f3979b87ba618285
gbelot <gbelot2003@hotmail.com>
2016-10-23 13:21:19 -0600

    Estableciendo modelos y migraciones de sustema de recomendaciones

---
+++ database/migrations/2016_10_23_192020_create_recomendations_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecomendationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('recomendations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('recomendations');
+    }
+}
