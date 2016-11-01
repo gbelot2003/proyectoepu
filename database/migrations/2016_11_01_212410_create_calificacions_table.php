6d4f61a40fb1ede32b76c35a01c1916ab1696518
gbelot <gbelot2003@hotmail.com>
2016-11-01 16:03:29 -0600

    estructurando calificación de recomendacion

---
+++ database/migrations/2016_11_01_212410_create_calificacions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCalificacionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calificacions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('recomendacion_id')->unsigned()->index();
+            $table->string('documento_url');
+            $table->integer('calificacion')->unsigned();
+            $table->text('details');
+            $table->integer('user_id')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('calificacions');
+    }
+}
